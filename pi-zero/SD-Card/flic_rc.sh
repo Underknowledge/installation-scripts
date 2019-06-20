3f7487672e1eef8dbb1459559a20ec969470b455
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-20 12:20:38 +0200

    Delete flic_rc.sh

--- pi-zero/SD-Card/flic_rc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sed -i "/ls -CF/ a alias bluetoothinfos='sudo btmon'" ~/.bashrc 
-sed -i "/ls -CF/ a fliclog='journalctl -u flicd -f'" ~/.bashrc 
-sed -i "/ls -CF/ a fliclogs='tail -f /home/pi/flic/flic_log.txt'" ~/.bashrc 
-sed -i "/ls -CF/ a alias simpleclient='/home/pi/simpleclient/simpleclient localhost'" ~/.bashrc 
