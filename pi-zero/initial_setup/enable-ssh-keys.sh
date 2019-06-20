9fb1c5badd87a2d2065f52b5c00502a3414887cc
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-20 11:50:54 +0200

    Update enable-ssh-keys.sh

--- pi-zero/initial_setup/enable-ssh-keys.sh
+++ pi-zero/initial_setup/enable-ssh-keys.sh
@@ -2,12 +2,13 @@
 ipadress=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 echo "========================================================"
 echo " This short script will disable password Authentication in the favur of keys  "
-echo " when you dont have ssh-keys yet I liked this article"
-echo " https://www.ssh.com/ssh/keygen/ " 
+echo " when you dont have ssh-keys yet I liked this article:"
+echo "       https://www.ssh.com/ssh/keygen/ " 
 echo " You can copy your key over with "
 echo   
 echo " \$ ssh-copy-id -i ~/.ssh/file.key.pub pi@${ipadress} "
 echo " high Chances that you named your file diffrently " 
+echo "========================================================"
 echo   
 echo " type yes to move on "
 read  
