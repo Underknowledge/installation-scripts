e0392b01aafac442e5b7695b58e2e2de35e7acc9
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-20 21:49:38 +0200

    Update bash_alias.sh

--- pi-zero/flicd/bash_alias.sh
+++ pi-zero/flicd/bash_alias.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 sed -i "/ls -CF/ a alias btinfo='sudo btmon'" ~/.bashrc 
 sed -i "/ls -CF/ a fliclog='journalctl -u flicd -f'" ~/.bashrc 
-sed -i "/ls -CF/ a fliclogs='tail -f /home/pi/flic/flic_log.txt'" ~/.bashrc 
+sed -i "/ls -CF/ a fliclogs='tail -f /var/log/flic_log'" ~/.bashrc 
 sed -i "/ls -CF/ a alias simpleclient='/home/pi/simpleclient/simpleclient localhost'" ~/.bashrc 
 sed -i "/ls -CF/ a alias startflic='sudo systemctl start flicd.service'" ~/.bashrc 
 sed -i "/ls -CF/ a alias stopflic='sudo systemctl stop flicd.service'" ~/.bashrc 
 sed -i "/ls -CF/ a alias restartflic='sudo systemctl restart flicd.service'" ~/.bashrc 
 sed -i "/ls -CF/ a alias enableflic='sudo systemctl enable flicd.service'" ~/.bashrc 
 sed -i "/ls -CF/ a alias disableflic='sudo systemctl disable flicd.service'" ~/.bashrc 
-sed -i "/ls -CF/ a alias flickill='sudo systemctl stop flicd.service && sudo rm /home/pi/flic/flic.sqlite3 && sudo reboot'" ~/.bashrc 
+sed -i "/ls -CF/ a alias flickill='sudo systemctl stop flicd.service && sudo rm /var/flic.db && sudo reboot'" ~/.bashrc
+sed -i "/ls -CF/ a \ " ~/.bashrc 
 exec bash
+
+
