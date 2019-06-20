c6cd59a9c34b003f2530c59a91a0b4fb3426c100
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-20 11:04:32 +0200

    Update flicd-installation-script.sh

--- pi-zero/flicd/flicd-installation-script.sh
+++ pi-zero/flicd/flicd-installation-script.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 mkdir /home/pi/flic/
-echo ""
+echo
 echo "Downloading  armv6l/flicd "
 curl https://raw.githubusercontent.com/50ButtonsEach/fliclib-linux-hci/master/bin/armv6l/flicd > /usr/local/bin/flicd
 chmod a+x /usr/local/bin/flicd
-echo ""
+echo
 echo "Downloading  systemd file and make it executable "
 curl https://raw.githubusercontent.com/Underknowledge/installation-scripts/pi-zero/flicd/flicd.service > /etc/systemd/system/flicd.service
 chmod a+x /etc/systemd/system/flicd.service
-echo ""
+echo
 echo "Disableing Bluetooth" 
 sudo systemctl stop bluetooth
 sudo systemctl disable bluetooth
-echo ""
+echo
 echo " enabling the new flicd.service" 
 sudo systemctl enable flicd.service
 sudo systemctl start flicd.service
-echo ""
-echo ""
+echo
+echo
 echo "creating the dir "
 mkdir ~/simpleclient
-echo ""
-echo ""
+echo
+echo
 echo "Downloading the sinple client" 
 curl https://raw.githubusercontent.com/50ButtonsEach/fliclib-linux-hci/master/simpleclient/Makefile > ~/simpleclient/Makefile
 curl https://raw.githubusercontent.com/50ButtonsEach/fliclib-linux-hci/master/simpleclient/simpleclient.cpp> ~/simpleclient/simpleclient.cpp
@@ -47,8 +47,8 @@ sed -i "/ls -CF/ a alias flicstop='sudo systemctl stop flicd.service'" ~/.bashrc
 sed -i "/ls -CF/ a alias flicrestart='sudo systemctl restart flicd.service'" ~/.bashrc 
 sed -i "/ls -CF/ a alias flicstatus='sudo systemctl status flicd.service'" ~/.bashrc 
 exec bash
-echo ""
-echo "run simpleclient with 'simpleclient` "
+echo
+echo " run simpleclient with 'simpleclient` "
 echo " Done " 
 echo " check for errors with 'journalctl -u flicd -f'  the alias is `fliclog` "
 echo " 'sudo btmon' for the actuall bt actions alias is `btinfo`"
