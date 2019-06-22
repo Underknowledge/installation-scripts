0bf70382efca185e2c27865a27b964ea4f876a64
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-22 12:34:20 +0200

    sudo for ifup

--- my_server/static_ip.sh
+++ my_server/static_ip.sh
@@ -3,10 +3,14 @@ eth_interface=$(ls /sys/class/net | grep enp)
 eth_interface_v2=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
 echo "========================================================"
 echo "    This short script will set the network interface from DHCP to static "
-echo "WIP - Not for use yet "
-echo "              Danger No route to host ! " 
+echo " "
+echo " " 
+echo " "
 echo "https://wiki.debian.org/NetworkConfiguration" 
-echo "   "
+echo
+echo "you can get your original configuation with: "
+echo "sudo cp /etc/network/interfaces_backup /etc/network/interfaces"
+echo
 echo "========================================================"
 echo
 echo "type yes to move on "
@@ -19,8 +23,13 @@ sudo sed -i "s=iface $(ls /sys/class/net | grep enp) inet dhcp=iface $(ls /sys/c
 sudo sed -i "/iface $(ls /sys/class/net | grep enp) inet static/ a \ \ \ \ \ \ \ \ gateway 10.0.0.1" /etc/network/interfaces
 sudo sed -i "/iface $(ls /sys/class/net | grep enp) inet static/ a \ \ \ \ \ \ \ \ netmask 255.255.255.0" /etc/network/interfaces
 sudo sed -i "/iface $(ls /sys/class/net | grep enp) inet static/ a \ \ \ \ \ \ \ \ address 10.0.0.27" /etc/network/interfaces
+echo "========================================================"
+echo "        we will be back in a short moment !  "
+echo "========================================================"
 sudo service networking restart
-ifup $(ls /sys/class/net | grep enp)
+sudo ifup $(ls /sys/class/net | grep enp)
+sleep 10
+echo
 echo "========================================================"
 echo "should be done now I ll send you to the file to check it now"
 echo "========================================================"
