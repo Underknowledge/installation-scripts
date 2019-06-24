b3c1de612b01da8bf9fbff62a3152b1d63218659
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-24 14:32:39 +0200

    Update flicd-installation-script.sh

--- pi-zero/flicd/flicd-installation-script.sh
+++ pi-zero/flicd/flicd-installation-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 hardware=$(uname -m)
 ipadress=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo 
-echo 
-echo "========================================================"
-echo "This short script will install a flic systemd deamon on a Raspberry Pi "
-echo "https://github.com/50ButtonsEach/fliclib-linux-hci" 
-echo ""
-echo "========================================================"
+echo "
+ 
+========================================================
+This short script will install a flic systemd deamon on a Raspberry Pi 
+https://github.com/50ButtonsEach/fliclib-linux-hci
+
+========================================================"
 echo 
 case $(ps -p 1 -o comm=) in
     systemd)  echo "systemd check passed" && echo && echo  ;;
@@ -34,25 +34,25 @@ sudo systemctl disable bluetooth
 echo "enabling the new flicd.service" 
 sudo systemctl enable flicd.service
 sudo systemctl start flicd.service
-echo "creating the dir "
-echo
-echo
+echo "creating the dir 
+
+"
 echo "setting up crontab" 
 (sudo crontab -u root -l; echo "@reboot systemctl stop bluetooth" ) | sudo crontab -u root -
 echo "creating resetflicdaemon alias"
 sed -i "/ls -CF/ a alias resetflicdaemon='sudo systemctl stop flicd.service && sudo rm /var/flic.db && sudo reboot'" ~/.bashrc 
 echo
 echo
-echo "you can add the following 3 lines to your Home Assistant config to use this pi as flic server" 
-echo "========================================================"
-echo "binary_sensor:"
-echo "  - platform: flic"
-echo "    host: ${ipadress} "
-echo "========================================================"
-echo
-echo
-echo "to pair a button just press it for +7 secconds" 
-echo "when you facing issues pairing, run 'resetflicdaemon' it will delete the database and reboot the pi"
+echo "you can add the following 3 lines to your Home Assistant config to use this pi as flic server
+========================================================
+binary_sensor:
+  - platform: flic
+    host: ${ipadress}
+========================================================
+
+
+to pair a button just press it for +7 secconds
+when you facing issues pairing, run 'resetflicdaemon' it will delete the database and reboot the pi"
 echo
 read -n 1 -s -r -p "Press any key to continue"
 echo 
