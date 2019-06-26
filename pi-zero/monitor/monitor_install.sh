11249f56043f54d7f94d9c59442643e9d4e0bb7b
Underknowledge <Wagner.beccard+github@gmail.com>
2019-06-26 10:41:14 +0200

    pre case

--- pi-zero/monitor/monitor_install.sh
+++ pi-zero/monitor/monitor_install.sh
@@ -7,29 +7,31 @@ This installs Mosquitto, git, and andrewjfreyer's Monitor without much cmd+c/v
 
 
 ========================================================"
-# get mosquitto repo key
+echo "# get mosquitto repo key"
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-#add repo
+echo "#add repo"
 sudo apt-key add mosquitto-repo.gpg.key
 rm mosquitto-repo.gpg.key
-#download appropriate lists file 
-cd /etc/apt/sources.list.d/
-sudo wget https://repo.mosquitto.org/debian/mosquitto-stretch.list
-# sudo curl https://repo.mosquitto.org/debian/mosquitto-stretch.list > /etc/apt/sources.list.d/mosquitto-stretch.list
-#update caches and install 
+echo "#download appropriate lists file"
+sudo wget -O /etc/apt/sources.list.d//mosquitto-$(lsb_release -cs).list https://repo.mosquitto.org/debian/mosquitto-$(lsb_release -cs).list
+echo "#update caches and install"
 apt-cache search mosquitto
 sudo apt-get update
-sudo apt-get install -f libmosquitto-dev mosquitto mosquitto-clients libmosquitto1 git
+sudo apt-get install -f libmosquitto-dev mosquitto mosquitto-clients libmosquitto1 git bc bluez-hcidump -y
 cd ~
-#clone andrewjfreyer's repo
+echo "#clone andrewjfreyer's repo"
 git clone git://github.com/andrewjfreyer/monitor
-#enter `monitor` directory
+echo "#enter `monitor` directory"
 cd monitor/
 echo " 
 
 Monitor will start up now and set up the service file 
 
-
 "
 delay 3
-sudo bash monitor.sh
+sudo bash /home/pi/monitor/monitor.sh
+
+sudo nano /etc/systemd/system/monitor.service
+
+sudo systemctl daemon-reload
+sudo systemctl restart monitor.service 
