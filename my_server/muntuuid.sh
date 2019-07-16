8cc4d38261daa1e83ea82d71bb89ecb681848117
Underknowledge <Wagner.beccard+github@gmail.com>
2019-07-16 10:08:32 +0200

    Create muntuuid.sh

---
+++ my_server/muntuuid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+uuid=AAA0-FFF0
+path=/media/usbxy
+
+if mount | grep $(blkid -U "${uuid}") | grep "${path}" > /dev/null
+then
+    echo "
+
+unmounting ${path}
+
+ "
+    sudo umount "${path}"
+    echo " Done "
+
+else
+    echo "
+
+couldn't find a mount in the filesystem
+trying to mount to ${path}  
+
+ "
+    sudo mount $(blkid -U "${uuid}") "${path}"
+    exit 0
+fi
