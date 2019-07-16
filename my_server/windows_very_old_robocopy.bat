fa66bfc6056cd3c7123dc71576a52e8cc1bd0964
Underknowledge <Wagner.beccard+github@gmail.com>
2019-07-16 10:37:30 +0200

    Update windows_very_old_robocopy.bat

--- my_server/windows_very_old_robocopy.bat
+++ my_server/windows_very_old_robocopy.bat
@@ -21,3 +21,21 @@ for /F "tokens=1*" %%a in ('fsutil fsinfo drives') do (
       )
    )
 )
+
+
+or from Tom Lavedas
+https://social.technet.microsoft.com/Forums/exchange/en-US/bc64aef0-e4c0-43fb-ad20-49951f3abde4/batch-script-to-detect-drive-letter-of-a-usb-flash-drive
+
+@echo off
+  if [%1]==[] echo Volume label input missing & goto :eof
+  setlocal
+  set n=0
+  set ltrs=ABCDEFGHIJKLMNOPQRSTUVWXYZ
+ :Loop
+  call set ltr=%%ltrs:~%n%,1%%
+  set /a n +=1
+  vol %ltr%: 2>nul|find /i " %1">nul||if %n% lss 26 goto :loop
+  if %n% equ 26 (set "ltr="
+    echo No matching volume label found.
+  ) else (echo Volume %1 is drive %ltr%:)
+  endlocal & set ltr=%ltr%
