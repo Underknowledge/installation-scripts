2759d0888acd34c8ac89288165739d104510c7e5
Underknowledge <Wagner.beccard+github@gmail.com>
2019-08-24 10:35:59 +0200

    Update adduser.sh

--- my_server/adduser.sh
+++ my_server/adduser.sh
@@ -4,6 +4,7 @@ if (( $# == 0 )); then
     exit1
 else
     echo "you will add an user named $1 and add them to the group $2"
+    echo " to add a group run 'sudo groupadd -g <GID you like> <group>' "
 fi
 echo "type yes to move on "
 read
@@ -13,4 +14,9 @@ fi
 sudo useradd -M $1 --shell /bin/false
 sudo usermod -L $1
 sudo usermod -a -G $2 $1
-echo " at the moment the users $(sudo awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow) are able to login "
+echo
+echo " at the moment the users 
+echo " $(sudo awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow) "
+echo "are able to login "
+echo
+echo " $(id $1)
